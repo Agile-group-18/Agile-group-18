91a2399259cf9cbb135192d4c039b37c4bd0fe55
Emil <emilstrom05@gmail.com>
2026-05-14 08:09:11 +0200

    fixa rebase mer

--- app/src/main/java/org/grupp18/sortsmart/MainActivity.kt
+++ app/src/main/java/org/grupp18/sortsmart/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import org.grupp18.sortsmart.ui.map.MapScreen
 import org.grupp18.sortsmart.ui.theme.SortSmartTheme
 
 /**
